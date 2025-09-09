3d2b7c80e1561af9dc14a2296dc3d3f1dd53c1f0
Brayan Gamboa <bsgv2005@gmail.com>
2025-09-08 23:34:46 -0500

    style(delete comments): delete comments

--- lib/infrastructure/webserver/server.ts
+++ lib/infrastructure/webserver/server.ts
@@ -20,8 +20,6 @@ const createServer = async () => {
     routes: {
       cors: {
         origin: ["*"],
-        // additionalHeaders: ["authorization", "content-type"],
-        // credentials: true // si necesitas enviar cookies/autenticación
       }
     }
   });
