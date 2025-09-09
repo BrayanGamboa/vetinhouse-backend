5267e3a5c071dc4ccd951ff9056242bdfe7f49a3
Brayan Gamboa <bsgv2005@gmail.com>
2025-09-08 22:28:58 -0500

    docs: update response schemas and validation

--- lib/application/schemas/auth/UserSchema.ts
+++ lib/application/schemas/auth/UserSchema.ts
@@ -13,14 +13,14 @@ export const UserPayloadSchema = Joi.object({
 export const UserResponseSchema = Joi.object({
   document: Joi.string().example("123456789"),
   name: Joi.string().example("Brayan"),
-  lastName: Joi.string().example("Gamboa"),
+  lastName: Joi.string().allow("").example("Gamboa"),
   email: Joi.string().example("brayan@email.com"),
   password: Joi.string().example("Hola mundo!#"),
   roleId: Joi.number().example(2),
   documentTypeId: Joi.number().example(1),
   info: Joi.object({
-    createdAt: Joi.string().example("2025-08-31T20:10:00Z"),
-    updatedAt: Joi.string().example("2025-08-31T20:10:00Z"),
+    createdAt: Joi.date().iso().example("2025-08-31T20:10:00Z"),
+    updatedAt: Joi.date().iso().example("2025-08-31T20:10:00Z"),
   })
 });
 
